d313d11895bc9ff05951d48bb7aa3681e14a4d58
BingYinBest <dreamfy66@outlook.com>
2026-09-08 23:31:21 +0800

    Fix: add missing import for dp in HomeScreen

--- app/src/main/java/com/bingyin/materialyouprefs/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/bingyin/materialyouprefs/ui/screens/HomeScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.bingyin.materialyouprefs.data.PrefData
 import com.bingyin.materialyouprefs.ui.components.PreferenceGroupSection
 
